18189261006e3008360634219a68bf325f51d12f
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-09-25 12:03:13 -0700

    Create practice1.py

---
+++ practice1.py
@@ -0,0 +1,12 @@
+Class Computer:
+	
+	def config(self):
+		print(“this is … machine with…) 
+
+com1 = Computer()
+com2 = Computer()
+
+Computer.config(com1)
+Computer.config(com2)
+com1.config()
+com2.config()
