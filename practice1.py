905a08b022b36df74cfe6330eabf7ac093dda871
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-09-25 12:16:14 -0700

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,12 +1,17 @@
-Class Computer:
+class Computer:
 	
-	def config(self):
-		print(“this is … machine with…) 
+	def __init__(self, cpu, ram):
+		self.cpu = cpu
+		self.ram = ram
 
-com1 = Computer()
-com2 = Computer()
+	def config(self):
+		print(“Config is “, self.cpu, self. Ram) 
+
+com1 = Computer(‘i5’, 16)
+com2 = Computer(‘Risen’, 8)
 
 Computer.config(com1)
 Computer.config(com2)
 com1.config()
 com2.config()
+
